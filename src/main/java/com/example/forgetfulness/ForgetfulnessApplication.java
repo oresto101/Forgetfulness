511aa602fbd34620993c3862e3412d5a8e7fd54f
Orest <oresto101@gmail.com>
2023-01-15 22:04:24 +0100

    Initial commit

---
+++ src/main/java/com/example/forgetfulness/ForgetfulnessApplication.java
@@ -0,0 +1,13 @@
+package com.example.forgetfulness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgetfulnessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForgetfulnessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/forgetfulness/ForgetfulnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forgetfulness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgetfulnessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
