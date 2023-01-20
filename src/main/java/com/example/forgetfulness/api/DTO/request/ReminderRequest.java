d0a82188b3040bd4e5907460d432ff16c33a30c6
Denys Demianov <demydeni@gmail.com>
2023-01-20 18:22:40 +0100

    Rename and fill DTO requests

---
+++ src/main/java/com/example/forgetfulness/api/DTO/request/ReminderRequest.java
@@ -0,0 +1,13 @@
+package com.example.forgetfulness.api.DTO.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ReminderRequest {
+    private Long id;
+    private String name;
+    private String description;
+    private Long recurrenceId;
+}
