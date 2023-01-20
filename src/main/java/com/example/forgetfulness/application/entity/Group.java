1242f55a997a97edb2fd12377f16b7c2899d9a2c
Denys Demianov <demydeni@gmail.com>
2023-01-20 18:02:44 +0100

    Initialize entities

--- src/main/java/com/example/forgetfulness/application/entity/Group.java
+++ src/main/java/com/example/forgetfulness/application/entity/Group.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
+import java.util.Set;
 
 @Table(name = "group_table")
 @Data
@@ -16,16 +16,18 @@ public class Group {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "description")
+    @Column(name = "description", nullable = false)
     private String description;
 
-    @Column(name = "users")
+    @OneToMany(mappedBy = "group")
+    private Set<UserGroup> members;
+
     @OneToMany
-    private List<User> users;
+    private Set<Reminder> reminders;
 }
