e13bdbf1e98844707ca15537e511a9a006bba76d
Denys Demianov <demydeni@gmail.com>
2023-01-21 22:23:29 +0100

    Add date field to Reminder request and response

--- src/main/java/com/example/forgetfulness/api/DTO/request/ReminderRequest.java
+++ src/main/java/com/example/forgetfulness/api/DTO/request/ReminderRequest.java
@@ -3,11 +3,14 @@ package com.example.forgetfulness.api.DTO.request;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @NoArgsConstructor
 public class ReminderRequest {
     private Long id;
     private String name;
     private String description;
+    private LocalDate date;
     private Long period;
 }
--- src/main/java/com/example/forgetfulness/api/DTO/response/ReminderResponse.java
+++ src/main/java/com/example/forgetfulness/api/DTO/response/ReminderResponse.java
@@ -3,10 +3,13 @@ package com.example.forgetfulness.api.DTO.response;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @NoArgsConstructor
 public class ReminderResponse {
     private String name;
     private String description;
+    private LocalDate date;
     private Long period;
 }
