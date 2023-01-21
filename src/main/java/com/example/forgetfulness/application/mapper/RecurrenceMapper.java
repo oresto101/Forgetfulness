0646dbb605e8771250d238c52b329c8b91309c51
Denys Demianov <demydeni@gmail.com>
2023-01-21 21:21:29 +0100

    Add mappers to responses

--- src/main/java/com/example/forgetfulness/application/mapper/RecurrenceMapper.java
+++ src/main/java/com/example/forgetfulness/application/mapper/RecurrenceMapper.java
@@ -1,10 +1,13 @@
 package com.example.forgetfulness.application.mapper;
 
 import com.example.forgetfulness.api.DTO.request.RecurrenceRequest;
+import com.example.forgetfulness.api.DTO.response.RecurrenceResponse;
 import com.example.forgetfulness.application.entity.Recurrence;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface RecurrenceMapper {
     Recurrence recurrenceRequestToRecurrence(RecurrenceRequest recurrenceRequest);
+
+    RecurrenceResponse recurrenceToRecurrenceResponse(Recurrence recurrence);
 }
--- src/main/java/com/example/forgetfulness/application/mapper/ReminderMapper.java
+++ src/main/java/com/example/forgetfulness/application/mapper/ReminderMapper.java
@@ -1,10 +1,13 @@
 package com.example.forgetfulness.application.mapper;
 
 import com.example.forgetfulness.api.DTO.request.ReminderRequest;
+import com.example.forgetfulness.api.DTO.response.ReminderResponse;
 import com.example.forgetfulness.application.entity.Reminder;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface ReminderMapper {
     Reminder reminderRequestToReminder(ReminderRequest reminderRequest);
+
+    ReminderResponse reminderToReminderResponse(Reminder reminder);
 }
