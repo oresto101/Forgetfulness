0baecc97a76abe0fa40ca0372c46b17ccfa78324
Denys Demianov <demydeni@gmail.com>
2023-01-21 22:49:58 +0100

    Return date as string

--- src/main/java/com/example/forgetfulness/api/DTO/response/ReminderResponse.java
+++ src/main/java/com/example/forgetfulness/api/DTO/response/ReminderResponse.java
@@ -3,13 +3,11 @@ package com.example.forgetfulness.api.DTO.response;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
 @Data
 @NoArgsConstructor
 public class ReminderResponse {
     private String name;
     private String description;
-    private LocalDate date;
+    private String date;
     private Long period;
 }
