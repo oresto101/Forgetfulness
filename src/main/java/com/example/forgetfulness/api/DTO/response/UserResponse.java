b7e93116d0762e7d642367eb97b8952490e2cbeb
Denys Demianov <demydeni@gmail.com>
2023-01-22 16:59:57 +0100

    Add ids to responses

--- src/main/java/com/example/forgetfulness/api/DTO/response/UserResponse.java
+++ src/main/java/com/example/forgetfulness/api/DTO/response/UserResponse.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponse {
+    private Long id;
     private String name;
     private String username;
     private String surname;
